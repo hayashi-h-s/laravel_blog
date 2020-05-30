60eef718dbf49396580a6e42ed81f0a0032d3824
haya <haya@appurunoMacBook-puro.local>
2020-05-30 10:26:04 +0900

    [add]articles.indexのビュー画面の　作成

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticlesController extends Controller
 {
@@ -13,7 +14,9 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        //
+      $articles = Article::all();
+    //   return $articles;
+      return view('articles.index', ['articles' => $articles]);
     }
 
     /**
---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,14 @@
+{{-- layoutsフォルダのapplication.blade.phpを継承 --}}
+@extends('layouts.app')
+
+{{-- @yield('title')にテンプレートごとの値を代入 --}}
+@section('title', '記事一覧')
+
+{{-- application.blade.phpの@yield('content')に以下のレイアウトを代入 --}}
+@section('content')
+  @foreach ($articles as $article)
+    <h4>{{$article->title}}</h4>
+    <p>{{$article->body}}</p>
+    <hr>
+  @endforeach
+@endsection
