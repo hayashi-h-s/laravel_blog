364cc2c7c001d949e4f8064e59673594b5758e85
haya <haya@appurunoMacBook-puro.local>
2020-05-31 20:03:24 +0900

    [add]articleのseedデータを作成

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -13,20 +13,59 @@ class ArticlesTableSeeder extends Seeder
     {
   // articlesテーブルにデータをinsert
         DB::table('articles')->insert([
+
             [
+            // 速水 信二 のデータ
             'title' => 'タイトル1',
             'body' => '内容1',
             'user_id' => '1'
             ],
+
+
             [
             'title' => 'タイトル2',
             'body' => '内容2',
             'user_id' => '1'
             ],
+
+
             [
             'title' => 'タイトル3',
             'body' => '内容3',
             'user_id' => '1'
+            ],
+
+            // テストユーザー1のデータ
+                        [
+            'title' => 'タイトル1',
+            'body' => '内容1',
+            'user_id' => '2'
+            ],
+            [
+            'title' => 'タイトル2',
+            'body' => '内容2',
+            'user_id' => '2'
+            ],
+            [
+            'title' => 'タイトル3',
+            'body' => '内容3',
+            'user_id' => '2'
+            ],
+            // テストユーザー2のデータ
+                        [
+            'title' => 'タイトル1',
+            'body' => '内容1',
+            'user_id' => '3'
+            ],
+            [
+            'title' => 'タイトル2',
+            'body' => '内容2',
+            'user_id' => '3'
+            ],
+            [
+            'title' => 'タイトル3',
+            'body' => '内容3',
+            'user_id' => '3'
             ],
         ]);
     }
