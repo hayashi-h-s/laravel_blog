fd1682be5dbb6f43ec5d261d31072ee1e757f806
haya <haya@appurunoMacBook-puro.local>
2020-06-01 06:49:10 +0900

    [fix]time_zoneを日本時間に変更

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+ use Carbon\Carbon;
 class ArticlesTableSeeder extends Seeder
 {
     /**
@@ -18,54 +18,74 @@ class ArticlesTableSeeder extends Seeder
             // 速水 信二 のデータ
             'title' => 'タイトル1',
             'body' => '内容1',
-            'user_id' => '1'
+            'user_id' => '1',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
 
 
             [
             'title' => 'タイトル2',
             'body' => '内容2',
-            'user_id' => '1'
+            'user_id' => '1',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
 
 
             [
             'title' => 'タイトル3',
             'body' => '内容3',
-            'user_id' => '1'
+            'user_id' => '1',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
 
             // テストユーザー1のデータ
                         [
             'title' => 'タイトル1',
             'body' => '内容1',
-            'user_id' => '2'
+            'user_id' => '2',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
+
             [
             'title' => 'タイトル2',
             'body' => '内容2',
-            'user_id' => '2'
+            'user_id' => '2',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
+
             [
             'title' => 'タイトル3',
             'body' => '内容3',
-            'user_id' => '2'
+            'user_id' => '2',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
             // テストユーザー2のデータ
                         [
             'title' => 'タイトル1',
             'body' => '内容1',
-            'user_id' => '3'
+            'user_id' => '3',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
             [
             'title' => 'タイトル2',
             'body' => '内容2',
-            'user_id' => '3'
+            'user_id' => '3',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
             [
             'title' => 'タイトル3',
             'body' => '内容3',
-            'user_id' => '3'
+            'user_id' => '3',
+            "created_at" => new DateTime(),
+            "updated_at" => new DateTime()
             ],
         ]);
     }
